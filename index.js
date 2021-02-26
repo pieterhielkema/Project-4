51b6e8500ff4a4a68c5900f32b6fef2cba187258
Pieter Hielkema <pieterhielkema@gmail.com>
2021-02-26 17:11:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,68 +57,9 @@ setInterval(() => {
             }
         },
         {
-            color: 'red',
-            x: 10,
-            y: 10,
-            width: 20,
-            height: 20,
-            params: {
-                i: 0,
-            },
-            callback: (item) => {
-                item.params.i++;
-                if(item.params.i > 5000) {
-                    map.objects = map.objects.filter(x => x !== item);
-                }
-            },
-            onUserInteraction: (player) => {
-                player.health -= 1;
-            }
-        },
-        {
             color: 'green',
             x: 200,
             y: 10,
-            width: 20,
-            height: 20,
-            params: {
-                i: 0,
-            },
-            callback: (item) => {
-                item.params.i++;
-                if(item.params.i > 5000) {
-                    map.objects = map.objects.filter(x => x !== item);
-                }
-            },
-            deleteAfterUse: true,
-            onUserInteraction: (player) => {
-                player.health = Math.min(100, player.health + 20);
-            }
-        },
-        {
-            color: 'green',
-            x: 200,
-            y: 400,
-            width: 20,
-            height: 20,
-            params: {
-                i: 0,
-            },
-            callback: (item) => {
-                item.params.i++;
-                if(item.params.i > 5000) {
-                    map.objects = map.objects.filter(x => x !== item);
-                }
-            },
-            deleteAfterUse: true,
-            onUserInteraction: (player) => {
-                player.health = Math.min(100, player.health + 20);
-            }
-        },
-        {
-            color: 'green',
-            x: 1000,
-            y: 600,
             width: 20,
             height: 20,
             params: {
@@ -173,6 +114,26 @@ setInterval(() => {
             deleteAfterUse: true,
             onUserInteraction: (player) => {
                 player.distance = Math.min(7, player.distance + 1);
+            }
+        },
+        {
+            color: 'indigo',
+            x: 1100,
+            y: 400,
+            width: 20,
+            height: 20,
+            params: {
+                i: 0,
+            },
+            callback: (item) => {
+                item.params.i++;
+                if(item.params.i > 5000) {
+                    map.objects = map.objects.filter(x => x !== item);
+                }
+            },
+            deleteAfterUse: true,
+            onUserInteraction: (player) => {
+                player.balls.regular += 10;
             }
         },
         {
